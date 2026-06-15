914416dbdb5b89956f9fd6159ffbe95710b87c86
Jamshid Tolegenov <jamshidtolegenov@gmail.com>
2026-06-15 12:49:09 +0500

    first commit

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Network Monitor Starter
-
-start "Backend" cmd /k "cd /d %~dp0backend && node server.js"
-
-timeout /t 2 >nul
-
-start "Frontend" cmd /k "cd /d %~dp0frontend && npm run dev -- --host"
